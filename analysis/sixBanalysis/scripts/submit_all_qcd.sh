2d7e7c27ed6295e32db2952c4a7a3bcb44777f64
Evan Koenig <ekoenig4@wisc.edu>
2021-07-13 07:47:52 -0500

    updated qcd submitting script

--- analysis/sixBanalysis/scripts/submit_all_qcd.sh
+++ analysis/sixBanalysis/scripts/submit_all_qcd.sh
@@ -1,14 +1,14 @@
-TAG="qcd_2018_sixb"
 ODIR="/store/user/ekoenig/6BAnalysis/NTuples/2018/QCD/"
 
-echo "... tag       : ", $TAG
+make exe -j || exit -1
+
 echo "... saving to : ", $ODIR
 
 qcd_files=$(ls input/Run2_UL/2018/QCD_Pt_*)
 
-make exe -j || exit -1
-
 for input in ${qcd_files[@]}; do
+    TAG=$(basename $input)
+    TAG=${TAG/.txt/""}
+    echo "... tag       : ", $TAG
     python scripts/submitSkimOnBatch.py --tag $TAG --outputDir $ODIR --cfg config/skim_ntuple_2018.cfg --njobs 100 --input $input
-    break
 done
