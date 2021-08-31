178a28f4af23954e582014fd3ad93a0567b034f4
Evan Koenig <ekoenig4@wisc.edu>
2021-08-31 10:28:56 -0500

    use signal file that isn't used in training, added models folder to tar

--- analysis/sixBanalysis/scripts/submit_all_signal.sh
+++ analysis/sixBanalysis/scripts/submit_all_signal.sh
@@ -8,7 +8,8 @@ make exe -j || exit -1
 echo "... tag       : ", $TAG
 echo "... saving to : ", $ODIR
 
-files=$(ls input/PrivateMC_2021/NMSSM_XYH_YToHH_6b_MX_700_MY_400_10M.txt )
+files=$(ls input/PrivateMC_2021/NMSSM_XYH_YToHH_6b_MX_700_MY_400.txt )
+#input/PrivateMC_2021/NMSSM_XYH_YToHH_6b_MX_700_MY_400_10M.txt
 
 for input in ${files[@]}; do
     python scripts/submitSkimOnBatch.py --tag $TAG --outputDir $ODIR --cfg $CFG --njobs 100 --input $input --is-signal
