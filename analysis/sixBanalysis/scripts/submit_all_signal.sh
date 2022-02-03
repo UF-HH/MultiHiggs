657708f08a0e6f02c4c610d3c55d716f1f24ec3a
Suzanne <srosenzweig09@gmail.com>
2022-02-03 15:24:02 -0500

    6b customization

--- analysis/sixBanalysis/scripts/submit_all_signal.sh
+++ analysis/sixBanalysis/scripts/submit_all_signal.sh
@@ -1,14 +1,14 @@
 # ODIR="/store/user/srosenzw/studies/"
-ODIR="/store/user/srosenzw/analysis/"
+ODIR="/store/user/srosenzw/sixb/sixb_ntuples/NMSSM/"
 
 . scripts/arg_submit.sh -v sr "$@"
 # TAG="${TAG}NMSSM"
 
 # CFG="config/skim_ntuple_2018_nocuts.cfg"
-# CFG="config/skim_ntuple_2018_btag.cfg"
-CFG="config/skim_ntuple_2018.cfg"
 # TAG="NMSSM_nocuts"
-TAG="NMSSM_Luca"
+
+CFG="config/skim_ntuple_2018.cfg"
+TAG="Summer2018UL"
 
 make exe -j || exit -1
 
@@ -40,11 +40,11 @@ echo "... saving to : ", $ODIR
 #     python scripts/submitSkimOnBatch.py --tag $TAG --outputDir $ODIR --cfg $CFG --njobs 100 --input $input --is-signal
 # done
 
-files=$(ls input/PrivateMC_2018/NMSSM_XYH_YToHH_6b_MX_700_MY_400.txt )
+# files=$(ls input/PrivateMC_2018/NMSSM_XYH_YToHH_6b_MX_700_MY_400.txt )
 # files=$(ls input/PrivateMC_2021/NMSSM_XYH_YToHH_6b_MX_700_MY_400_10M.txt )
-for input in ${files[@]}; do
-    python scripts/submitSkimOnBatch.py --tag $TAG --outputDir $ODIR --cfg $CFG --njobs 100 --input $input --is-signal
-done
+# for input in ${files[@]}; do
+    # python scripts/submitSkimOnBatch.py --tag $TAG --outputDir $ODIR --cfg $CFG --njobs 100 --input $input --is-signal
+# done
 
 # files=$(ls input/PrivateMC_2018/NMSSM_XYH_YToHH_6b_MX_700_MY_500.txt )
 # for input in ${files[@]}; do
