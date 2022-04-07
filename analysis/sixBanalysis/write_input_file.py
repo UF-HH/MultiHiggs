56b47f4c70556799d6f2566ef418427d14c45aa3
Suzanne <srosenzweig09@gmail.com>
2022-04-07 10:42:55 -0400

    Updated output location.

--- analysis/sixBanalysis/write_input_file.py
+++ analysis/sixBanalysis/write_input_file.py
@@ -8,6 +8,7 @@ This script will populate a text file with all ROOT files available in a given C
 from argparse import ArgumentParser
 import os
 import re
+import shlex
 import subprocess
 import sys
 
@@ -15,10 +16,12 @@ print(".. parsing argument line")
 parser = ArgumentParser(description='Command line parser of model options and tags')
 parser.add_argument('--year', dest='year', help='conditions of which year', required=True)
 parser.add_argument('--dir', dest='dir', help='CRAB output directory', required=True)
+parser.add_argument('-a', '--analysis', dest='analysis', help='6b or 8b', required=True)
 
 args = parser.parse_args()
 
 dirName = args.dir
+if dirName[-1] != '/': dirName = dirName + '/'
 
 # find the desired file name, NMSSM_XYH_YToHH_6b_MX_###_MY_###
 # Assumption: crab job was saved in the format
@@ -33,17 +36,17 @@ def exists_on_eos(lfn):
     # print "THE FOLDER", lfn, "RETURNED CODE", retcode
     return True if retcode == 0 else False
 
-outputName = f"input/PrivateMC_{args.year}/{textfile}"
+outputName = f"input/PrivateMC_{args.year}/NMSSM_XYY_YToHH_{args.analysis}/{textfile}"
 with open(outputName, "w") as f:
     print(f".. writing to file: {outputName}")
     if (exists_on_eos(dirName)):
-        output = subprocess.run(["eos", "root://cmseos.fnal.gov", "ls", dirName], capture_output=True)
-        listOfDirs = output.stdout.decode("utf-8").split("\n")
+        output = subprocess.check_output(shlex.split(f"eos root://cmseos.fnal.gov ls {dirName}"))
+        listOfDirs = output.decode("utf-8").split("\n")
         for eachDir in listOfDirs:
             if eachDir != '':
                 fullPath = dirName + eachDir
-                output = subprocess.run(["eos", "root://cmseos.fnal.gov", "ls", fullPath], capture_output=True)
-                listOfFiles = output.stdout.decode("utf-8").split("\n")
+                output = subprocess.check_output(["eos", "root://cmseos.fnal.gov", "ls", fullPath])
+                listOfFiles = output.decode("utf-8").split("\n")
                 for fileName in listOfFiles:
                     # print(fileName)
                     if fileName != '':
