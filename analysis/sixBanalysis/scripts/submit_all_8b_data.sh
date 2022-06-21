ec2e1175abc179157b6346cdcc7cfeff9c3e7ee1
Evan Koenig <ekoenig4@wisc.edu>
2022-06-21 09:55:50 -0500

    updating run scripts

--- analysis/sixBanalysis/scripts/submit_all_8b_data.sh
+++ analysis/sixBanalysis/scripts/submit_all_8b_data.sh
@@ -2,7 +2,7 @@ ODIR="/store/user/ekoenig/8BAnalysis/NTuples/2018/"
 # ODIR="/store/user/srosenzw/analysis/"
 
 . scripts/arg_submit.sh -v qcd "$@"
-TAG="QCD"
+TAG="JetHT_Data"
 CFG="config/skim_ntuple_2018_8b.cfg"
 
 make exe -j || exit -1
@@ -10,8 +10,8 @@ make exe -j || exit -1
 echo "... tag       : ", $TAG
 echo "... saving to : ", $ODIR
 
-qcd_files=$(ls input/Run2_Autumn18/QCD*BGenFilter* input/Run2_UL/2018/QCD*bEnriched*)
+qcd_files=$(ls input/Run2_UL/2018/JetHT*)
 
 for input in ${qcd_files[@]}; do
-    python scripts/submitSkimOnBatch.py --tag $TAG --outputDir $ODIR --cfg $CFG --njobs 100 --input $input --memory 2500 --no-genw-tree
+    python scripts/submitSkimOnBatch.py --tag $TAG --outputDir $ODIR --cfg $CFG --njobs 200 --input $input --is-data
 done
