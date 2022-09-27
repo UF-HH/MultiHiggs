55bef4e4179cc96c90f0a48ca7a0114d577de07c
Evan Koenig <ekoenig4@wisc.edu>
2022-09-27 07:00:36 -0500

    output directory needs to be a store

--- analysis/sixBanalysis/scripts/8b_scripts/submit_all_8b_signal.sh
+++ analysis/sixBanalysis/scripts/8b_scripts/submit_all_8b_signal.sh
@@ -11,7 +11,7 @@ indir=$with_pu
 # ODIR="/eos/uscms/store/user/ekoenig/8BAnalysis/NTuples/2018/accstudies/raw_no_pu"
 
 CFG="config/8b_config/skim_ntuple_2018_t8btag_minmass.cfg"
-ODIR="/eos/uscms/store/user/ekoenig/8BAnalysis/NTuples/2018/preselection/t8btag_minmass/"
+ODIR="/store/user/ekoenig/8BAnalysis/NTuples/2018/preselection/t8btag_minmass/"
 
 TAG="NMSSM_XYY_YToHH_8b"
 
