adc637d324a5db9643932ce77ffbbedcc88f0d19
Marina Kolosova <mkolosov@cern.ch>
2022-11-03 05:34:36 -0500

    Added remaining PU weights for RunIISummer20UL18NanoAODv9 samples

--- analysis/sixBanalysis/runPUWeights.sh
+++ analysis/sixBanalysis/runPUWeights.sh
@@ -48,6 +48,24 @@
 #./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/srosenzw_NMSSM_XYH_YToHH_6b_MX_1000_MY_300_sl7_nano_100k.txt --output weights/2018/NMSSM_XYH_YToHH_6b_MX_1000_MY_300_PUweights.root
 #./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/TTJets.txt --output weights/2018/TTJets_PUweights.root
 
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_HT100to200_BGenFilter_TuneCP5_13TeV-madgraph-pythia8.txt --output weights/2018/QCD_HT100to200_BGenFilter_TuneCP5_13TeV-madgraph-pythia8_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_HT200to300_BGenFilter_TuneCP5_13TeV-madgraph-pythia8.txt --output weights/2018/QCD_HT200to300_BGenFilter_TuneCP5_13TeV-madgraph-pythia8_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_HT300to500_BGenFilter_TuneCP5_13TeV-madgraph-pythia8.txt --output weights/2018/QCD_HT300to500_BGenFilter_TuneCP5_13TeV-madgraph-pythia8_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_HT500to700_BGenFilter_TuneCP5_13TeV-madgraph-pythia8.txt --output weights/2018/QCD_HT500to700_BGenFilter_TuneCP5_13TeV-madgraph-pythia8_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_HT700to1000_BGenFilter_TuneCP5_13TeV-madgraph-pythia8.txt --output weights/2018/QCD_HT700to1000_BGenFilter_TuneCP5_13TeV-madgraph-pythia8_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_HT1000to1500_BGenFilter_TuneCP5_13TeV-madgraph-pythia8.txt --output weights/2018/QCD_HT1000to1500_BGenFilter_TuneCP5_13TeV-madgraph-pythia8_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_HT1500to2000_BGenFilter_TuneCP5_13TeV-madgraph-pythia8.txt --output weights/2018/QCD_HT1500to2000_BGenFilter_TuneCP5_13TeV-madgraph-pythia8_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/Run2_UL/RunIISummer20UL18NanoAODv9/QCD_HT2000toInf_BGenFilter_TuneCP5_13TeV-madgraph-pythia8.txt --output weights/2018/QCD_HT2000toInf_BGenFilter_TuneCP5_13TeV-madgraph-pythia8_PUweights.root
+
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_900_MY_400.txt --output weights/2018/NMSSM_XYY_YToHH_8b_MX_900_MY_400_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_900_MY_300.txt --output weights/2018/NMSSM_XYY_YToHH_8b_MX_900_MY_300_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_800_MY_350.txt --output weights/2018/NMSSM_XYY_YToHH_8b_MX_800_MY_350_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_800_MY_300.txt --output weights/2018/NMSSM_XYY_YToHH_8b_MX_800_MY_300_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_700_MY_300.txt --output weights/2018/NMSSM_XYY_YToHH_8b_MX_700_MY_300_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_1200_MY_500.txt --output weights/2018/NMSSM_XYY_YToHH_8b_MX_1200_MY_500_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_1000_MY_450.txt --output weights/2018/NMSSM_XYY_YToHH_8b_MX_1000_MY_450_PUweights.root
+#./bin/getPUWeights.exe --realPU weights/2018/Collision18PileupHistogram.root --input input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_1000_MY_300.txt --output weights/2018/NMSSM_XYY_YToHH_8b_MX_1000_MY_300_PUweights.root
+
 realPU="weights/2018/Collision18PileupHistogram.root"
 output="weights/2018/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_PUweights.root"
 input="input/Run2_UL/RunIISummer20UL18NanoAODv9/TTTo2L2Nu.txt"
