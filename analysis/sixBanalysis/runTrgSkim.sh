df418081114c6330eb6fa8c97bdb8fc79e8ba2b7
Marina Kolosova <mkolosov@cern.ch>
2022-11-28 06:53:22 -0600

    Added config for 2017 trigger efficiency measurement

--- analysis/sixBanalysis/runTrgSkim.sh
+++ analysis/sixBanalysis/runTrgSkim.sh
@@ -1,28 +1,57 @@
 #!/bin/sh
 
-cfg="config/skim_trigger_2018_106X_NanoAODv9.cfg"
 exe=bin/skim_trigger.exe
 
-#------------------------------------------------------------------------
-# Run Data
-#------------------------------------------------------------------------
-output="SingleMuonA_TrgStudies.root"
-input="input/Run2_UL/RunIISummer20UL18NanoAODv9/SingleMuon_Run2018A.txt"
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# 2017
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+cfg="config/skim_trigger_2017_106X_NanoAODv9.cfg"
 
+# Run 2017 Data
+#output="SingleMuonB_2017_TrgStudies.root"
+#input="input/Run2_UL/RunIISummer20UL17NanoAODv9/SingleMuon_Run2017B.txt"
+#make exe -j && \
+#    $exe \
+#    --input $input \
+#    --cfg  $cfg \
+#    --output $output \
+#    --is-data \
+#    --match \
+#    --maxEvts 200000 \
+#    $@
+
+# Run 2017 TTTo2L2Nu
+output="TTTo2L2Nu_2017_TrgStudies.root"
+input="input/Run2_UL/RunIISummer20UL17NanoAODv9/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8.txt"
 make exe -j && \
     $exe \
     --input $input \
     --cfg  $cfg \
     --output $output \
-    --is-data \
     --match \
-    --maxEvts 200000 \
+    --maxEvts 20000 \
     $@
 
-#------------------------------------------------------------------------
-# Run TTTo2L2Nu
-#------------------------------------------------------------------------
-#output="TTTo2L2Nu_TrgStudies.root"
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# 2018
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#cfg="config/skim_trigger_2018_106X_NanoAODv9.cfg"
+
+# Run 2018 Data
+#output="SingleMuonA_2018_TrgStudies.root"
+#input="input/Run2_UL/RunIISummer20UL18NanoAODv9/SingleMuon_Run2018A.txt"
+#make exe -j && \
+#    $exe \
+#    --input $input \
+#    --cfg  $cfg \
+#    --output $output \
+#    --is-data \
+#    --match \
+#    --maxEvts 200000 \
+#    $@
+
+# Run 2018 TTTo2L2Nu
+#output="TTTo2L2Nu_2018_TrgStudies.root"
 #input="input/Run2_UL/RunIISummer20UL18NanoAODv9/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8.txt"
 #make exe -j && \
 #    $exe \
@@ -32,10 +61,9 @@ make exe -j && \
 #    --match \
 #    --maxEvts 20000 \
 #    $@
-#------------------------------------------------------------------------
-# Run Signal
-#------------------------------------------------------------------------
-#output="NMSSM_XYH_YToHH_6b_MX_800_MY_300_TrgStudies.root"
+
+# Run 2018 Signal
+#output="NMSSM_XYH_YToHH_6b_MX_800_MY_300_2018_TrgStudies.root"
 #input="input/PrivateMC_2018/srosenzw_NMSSM_XYH_YToHH_6b_MX_800_MY_300_sl7_nano_100k.txt"
 #make exe -j && \
 #    $exe \
