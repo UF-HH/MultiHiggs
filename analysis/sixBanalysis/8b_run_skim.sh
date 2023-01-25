c0ef9626ace1268e13d4b51798353faa15e3de69
Evan Koenig <ekoenig4@wisc.edu>
2023-01-25 10:49:58 -0600

    updating 8b scripts

--- analysis/sixBanalysis/8b_run_skim.sh
+++ analysis/sixBanalysis/8b_run_skim.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 output="output.root"
-input="input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_1000_MY_450.txt --is-signal"
 
-# cfg="config/8b_config/accstudies_2018.cfg"
+input="input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_1000_MY_450.txt --is-signal"
 cfg="config/8b_config/skim_ntuple_2018_t8btag_minmass.cfg"
-# cfg="config/8b_config/skim_ntuple_2018_ranked_quadh_new.cfg"
      
+input="input/Run2_UL/RunIISummer20UL16NanoAODv9/QCD_bEnriched_HT700to1000_TuneCP5_13TeV-madgraph-pythia8.txt"
+cfg="config/8b_config/skim_ntuple_2016_t8btag_minmass.cfg"
+
 make exe -j && \
     ./bin/skim_ntuple.exe \
 	--input $input \
