f865c6ae84beb43a5b156f2ad2dc4ce6fe4bc6aa
Evan Koenig <ekoenig4@wisc.edu>
2023-01-30 12:04:11 -0600

    should use default option with new configs

--- analysis/MultiHAnalysis/8b_run_skim.sh
+++ analysis/MultiHAnalysis/8b_run_skim.sh
@@ -5,8 +5,8 @@ output="output.root"
 input="input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_1000_MY_450.txt --is-signal"
 cfg="config/8b_config/skim_ntuple_2018_t8btag_minmass.cfg"
      
-input="input/Run2_UL/RunIISummer20UL16NanoAODv9/QCD_bEnriched_HT700to1000_TuneCP5_13TeV-madgraph-pythia8.txt"
-cfg="config/8b_config/skim_ntuple_2016_t8btag_minmass.cfg"
+# input="input/Run2_UL/RunIISummer20UL16NanoAODv9/QCD_bEnriched_HT700to1000_TuneCP5_13TeV-madgraph-pythia8.txt"
+# cfg="config/8b_config/skim_ntuple_2016_t8btag_minmass.cfg"
 
 make exe -j && \
     ./bin/skim_ntuple.exe \
