a280163290d418bcd65a3f6e1dfa079b3ae3a71c
Evan Koenig <ekoenig4@wisc.edu>
2023-02-03 12:42:15 -0600

    updating 8b scripts

--- analysis/MultiHAnalysis/8b_run_skim.sh
+++ analysis/MultiHAnalysis/8b_run_skim.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-output="output.root"
 
+output="NMSSM_XYY_YToHH_8b_MX_1000_MY_450_test.root"
 input="input/PrivateMC_2018/NMSSM_XYY_YToHH_8b/NMSSM_XYY_YToHH_8b_MX_1000_MY_450.txt --is-signal"
 cfg="config/8b_config/skim_ntuple_2018_t8btag_minmass.cfg"
      
+	 
+# output="QCD_bEnriched_HT700to1000_test.root"
 # input="input/Run2_UL/RunIISummer20UL16NanoAODv9/QCD_bEnriched_HT700to1000_TuneCP5_13TeV-madgraph-pythia8.txt"
 # cfg="config/8b_config/skim_ntuple_2016_t8btag_minmass.cfg"
 
--- analysis/MultiHAnalysis/scripts/8b_scripts/submit_all_8b_qcd.sh
+++ analysis/MultiHAnalysis/scripts/8b_scripts/submit_all_8b_qcd.sh
@@ -1,15 +1,15 @@
 
 ODIR="/store/user/ekoenig/8BAnalysis/NTuples/2018/preselection/t8btag_minmass/"
 
-VERSION="Run2_UL/RunIISummer20UL16NanoAODv9"
-CFG="config/8b_config/skim_ntuple_2016_t8btag_minmass.cfg"
+# VERSION="Run2_UL/RunIISummer20UL16NanoAODv9"
+# CFG="config/8b_config/skim_ntuple_2016_t8btag_minmass.cfg"
 
 # VERSION="Run2_UL/RunIISummer20UL17NanoAODv9"
 # CFG="config/8b_config/skim_ntuple_2017_t8btag_minmass.cfg"
 
-# VERSION="Run2_UL/RunIISummer20UL18NanoAODv9"
+VERSION="Run2_UL/RunIISummer20UL18NanoAODv9"
 # VERSION="Run2_Autumn18"
-# CFG="config/8b_config/skim_ntuple_2018_t8btag_minmass.cfg"
+CFG="config/8b_config/skim_ntuple_2018_t8btag_minmass.cfg"
 
 TAG="$VERSION/QCD/"
 
