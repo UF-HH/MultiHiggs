39721c4747b8211d8f0524f7297493d1ac6a0f05
evan koenig <ekoenig@wisc.edu>
2024-02-06 02:58:18 -0800

    fixed jetht sample names

--- analysis/MultiHAnalysis/samples/analysis/2018/sixb_samples.py
+++ analysis/MultiHAnalysis/samples/analysis/2018/sixb_samples.py
@@ -73,10 +73,10 @@ merged_samples.update(samples)
 from metis.Sample import DirectorySample, DBSSample
 
 samples = {
-"JetHT" : DBSSample(dataset="/JetHT/Run2018A-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"),
-"JetHT" : DBSSample(dataset="/JetHT/Run2018B-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"),
-"JetHT" : DBSSample(dataset="/JetHT/Run2018C-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"),
-"JetHT" : DBSSample(dataset="/JetHT/Run2018D-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+"JetHT_Run2018A" : DBSSample(dataset="/JetHT/Run2018A-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"),
+"JetHT_Run2018B" : DBSSample(dataset="/JetHT/Run2018B-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+"JetHT_Run2018C" : DBSSample(dataset="/JetHT/Run2018C-UL2018_MiniAODv2_NanoAODv9-v1/NANOAOD"),
+"JetHT_Run2018D" : DBSSample(dataset="/JetHT/Run2018D-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD"),
 }
 
 merged_samples.update(samples)
